c07159adc3d5796ac37cab518e5e93cba3ca9cd9
BrunaReginat0 <bruna.reginato@sptech.school>
2024-06-10 23:02:38 -0300

    ✨ adicionei criptografia

--- hdp/src/models/funcionarioModel.js
+++ hdp/src/models/funcionarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT * FROM funcionario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM funcionario WHERE email = '${email}' AND senha = MD5('${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -16,7 +16,7 @@ function cadastrar(fkEmpresa, nome, cpf, telefone, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO funcionario (fkEmpresa, nome, cpf, telefone, email, senha) VALUES ('${fkEmpresa}','${nome}', '${cpf}', '${telefone}', '${email}', '${senha}');
+        INSERT INTO funcionario (fkEmpresa, nome, cpf, telefone, email, senha) VALUES ('${fkEmpresa}','${nome}', '${cpf}', '${telefone}', '${email}', MD5('${senha}'));
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
